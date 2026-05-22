67304144063b42fd30799ab9417b7fc7796d6e4f
Datkep92 <nguyenquocdat1511992@gmail.com>
2026-05-23 01:35:11 +0700

    Add files via upload

--- core.js
+++ core.js
@@ -201,3 +201,37 @@ function isAddable(date){
   return false;
 }
 
+/* =========================
+   BLOCK MOBILE ZOOM
+========================= */
+
+document.addEventListener(
+  "gesturestart",
+  function(e){
+
+    e.preventDefault();
+
+  }
+);
+
+
+
+let lastTouchEnd = 0;
+
+document.addEventListener(
+  "touchend",
+  function(e){
+
+    const now = Date.now();
+
+    if(now - lastTouchEnd <= 300){
+
+      e.preventDefault();
+
+    }
+
+    lastTouchEnd = now;
+
+  },
+  { passive:false }
+);
